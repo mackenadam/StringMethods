8944c1512faaa314d8b9018ab6acd1ddea6a94f7
Adam Macken <mackenadam@gmail.com>
2021-02-16 15:37:20 +1300

    Comments

--- StringMethods/Program.cs
+++ StringMethods/Program.cs
@@ -6,14 +6,18 @@ namespace StringMethods
     {
         static void Main(string[] args)
         {
+            // Take a string as an input
             Console.Write("Please input a string:");
             string inputString = Console.ReadLine();
             Console.WriteLine("You input: {0}", inputString);
+
+            // Take a character to find within the string
             Console.Write("Type a character to find the index:");
             char inputCharacter = (char)Console.Read();
             Console.WriteLine("You input : {0}", inputCharacter);
             int characterIndex = inputString.IndexOf(inputCharacter);
             Console.WriteLine("{0} can be found at index: {1}", inputCharacter, characterIndex);
+
         }
     }
 }
