f64cb12592d8d5c07bd74c8e26109a56e9896484
Adam Macken <mackenadam@gmail.com>
2021-02-16 15:29:39 +1300

    Add project files.

---
+++ StringMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input a string:");
+            string inputString = Console.ReadLine();
+            Console.WriteLine("You input: {0}", inputString);
+            Console.Write("Type a character to find the index:");
+            char inputChar = (char)Console.Read();
+            Console.WriteLine("You input : {0}", inputChar);
+        }
+    }
+}
