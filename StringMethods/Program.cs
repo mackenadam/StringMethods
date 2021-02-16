ec854c9d484c680620f1949747cb1252ce8904a8
Adam Macken <mackenadam@gmail.com>
2021-02-16 15:51:10 +1300

    Change Console.Read to Console.ReadLine to fix input stream issue.

--- StringMethods/Program.cs
+++ StringMethods/Program.cs
@@ -13,11 +13,20 @@ namespace StringMethods
 
             // Take a character to find within the string
             Console.Write("Type a character to find the index:");
-            char inputCharacter = (char)Console.Read();
+            string inputCharacter = Console.ReadLine();
             Console.WriteLine("You input : {0}", inputCharacter);
             int characterIndex = inputString.IndexOf(inputCharacter);
             Console.WriteLine("{0} can be found at index: {1}", inputCharacter, characterIndex);
 
+            // Take first and last names as seperate inputs
+            Console.Write("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Your first name has been saved as " + firstName + ".");
+
+            Console.Write("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Your last name has been saved as " + lastName + ".");
+            Console.WriteLine("Thank you, {0}. Your full name has been saved as {0} {1}.", firstName, lastName);
         }
     }
 }
